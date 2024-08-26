e82f6de9eb434c060b4b097f0a4db13ee5c95cd6
Rohan Thapa <rohanthapa632@gmail.com>
2024-08-26 13:23:23 +0545

    Update farey-algorithm.py
    Adding type annotation in the file for better understanding and promoting the good habits.

--- farey-algorithm.py
+++ farey-algorithm.py
@@ -1,14 +1,24 @@
 import math
+from typing import Union
 
-def fraction(number, precision):
+def fraction(number: float, precision: int) -> Union[str, None]:
+    pnts: float
+    num: float
     pnts, num = math.modf(number)
-    round_to = len(str(number).split('.')[1])
+    
+    round_to: int = len(str(number).split('.')[1])
     pnts = round(pnts, round_to)
-    a1, a2, b1, b2 = 1, 1, 0, 1
-    numerator, denominator = a1 + b1, a2 + b2
+    
+    a1: int = 1
+    a2: int = 1
+    b1: int = 0
+    b2: int = 1
+    
+    numerator: int = a1 + b1
+    denominator: int = a2 + b2
     
     while precision >= (math.ceil(math.log(denominator, 10))):
-        median = numerator / denominator
+        median: float = numerator / denominator
         
         if median > pnts:
             a1 = numerator
@@ -17,18 +27,18 @@ def fraction(number, precision):
             b1 = numerator
             b2 = denominator
         else:
-            return (str((int(num) * denominator) + numerator) + " / " + str(denominator))
+            return str((int(num) * denominator) + numerator) + " / " + str(denominator)
             
         numerator, denominator = a1 + b1, a2 + b2
         
-    # the condition for the value is a1/a2 > pnts > b1/b2
-    temp1, temp2 = (a1/a2) - pnts, pnts - (b1/b2)
+    temp1: float = (a1 / a2) - pnts
+    temp2: float = pnts - (b1 / b2)
     
     if (temp1 - temp2) > 0:
-        return (str((int(num) * b2) + b1)) +" / "+ str(b2)
+        return str((int(num) * b2) + b1) + " / " + str(b2)
     elif (temp1 - temp2) < 0:
-        return (str((int(num) * a2) + a1)) +" / "+ str(a2)
+        return str((int(num) * a2) + a1) + " / " + str(a2)
     else:
-        return "An Error had occured while calculating!"
-        
+        return "An Error occurred while calculating!"
+
 print(fraction(126.58215, 3))
